94317a378942453ec8bba4a66bde4673d63b6c2c
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-03-23 05:01:07 -0700

    Create 23.cpp

---
+++ gfg/march/23/23.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int mod = 1000000007;
+    vector<int> Series(int n) {
+        // Code here
+        vector<int> ans(n+1);
+        ans[0] = 0;
+        ans[1] = 1;
+        for(int i=2; i<=n; i++) {
+            ans[i] = (ans[i-1] + ans[i-2])%mod;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution obj;
+
+        vector<int> ans = obj.Series(n);
+        for (auto x : ans) cout << x << " ";
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
