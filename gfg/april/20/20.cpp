b4dc36cbd820f3e8601714c0ba5cce645caee6d2
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-04-19 18:18:46 -0700

    Create 20.cpp

---
+++ gfg/april/20/20.cpp
@@ -0,0 +1,80 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    //arr1,arr2 : the arrays
+    // n, m: size of arrays
+    //Function to return a list containing the union of the two arrays. 
+    vector<int> findUnion(int arr1[], int arr2[], int n, int m)
+    {
+        //Your code here
+        //return vector with correct order of elements
+        vector<int> ans;
+        int i = 0;
+        int j = 0;
+        int k = -1;
+        while(i<n && j<m) {
+            if(arr1[i] < arr2[j]) ans.push_back(arr1[i++]);
+            else ans.push_back(arr2[j++]);
+            k++;
+            while(ans[k] == arr1[i]) i++;
+            while(ans[k] == arr2[j]) j++;
+        }
+        while(i < n) {
+            if(k != -1) {
+                while(ans[k] == arr1[i]) i++;
+                if(i >= n) break;
+            }
+            ans.push_back(arr1[i++]);
+            k++;
+        }
+        while(j < m) {
+            if(k != -1) {
+                while(ans[k] == arr2[j]) j++;
+                if(j >= m) break;
+            }
+            ans.push_back(arr2[j++]);
+            k++;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	int T;
+	cin >> T;
+	
+	while(T--){
+	    
+	    
+	    
+	    int N, M;
+	    cin >>N >> M;
+	    
+	    int arr1[N];
+	    int arr2[M];
+	    
+	    for(int i = 0;i<N;i++){
+	        cin >> arr1[i];
+	    }
+	    
+	    for(int i = 0;i<M;i++){
+	        cin >> arr2[i];
+	    }
+	    Solution ob;
+	    vector<int> ans = ob.findUnion(arr1,arr2, N, M);
+	    for(int i: ans)cout<<i<<' ';
+	    cout << endl;
+	    
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
