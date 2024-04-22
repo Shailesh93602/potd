148ddc7145906c482c8dc01d0adc62766425aef7
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-04-21 17:51:53 -0700

    Create 22.cpp

---
+++ gfg/april/22/22.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int minRow(int n, int m, vector<vector<int>> a) {
+        // code here
+        int row = 0;
+        int minCount = INT_MAX;
+        for(int i=0; i<a.size(); i++) {
+            int count = 0;
+            for(int j=0; j<a[i].size(); j++) {
+                if(a[i][j] == 1) count++;
+            }
+            if(count < minCount) {
+                minCount = count;
+                row = i+1;
+            }
+        }
+        return row;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, M;
+        cin >> N >> M;
+        vector<vector<int>> A(N, vector<int>(M));
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                cin >> A[i][j];
+            }
+        }
+        Solution ob;
+        cout << ob.minRow(N, M, A) << "\n";
+    }
+}
+// } Driver Code Ends
