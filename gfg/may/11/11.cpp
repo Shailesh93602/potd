e94fceab993813f994fa94090b06dd0fe2dcff3e
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-05-10 20:08:49 -0700

    Create 11.cpp

---
+++ gfg/may/11/11.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<int> jugglerSequence(int n) {
+        // code here
+        vector<int> ans;
+        ans.push_back(n);
+        while(n > 1) {
+            if(n%2 == 0) n = sqrt(n);
+            else n = n*sqrt(n);
+            ans.push_back(n);
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        vector<int> ans = ob.jugglerSequence(n);
+        for (int u : ans)
+            cout << u << " ";
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
