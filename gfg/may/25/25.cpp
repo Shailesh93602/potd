a468e217711f662215e665f480bbf21f3d017e34
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-05-24 20:15:04 -0700

    Create 25.cpp

---
+++ gfg/may/25/25.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <algorithm>
+#include <cstdio>
+#include <iostream>
+//#include<Windows.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    /*You are requried to complete this method */
+    long long max_Books(int arr[], int n, int k) {
+        // Your code here
+        long long maxSum = 0;
+        long long sum = 0;
+        for(int i=0; i<n; i++) {
+            if(arr[i] <= k) sum += arr[i];
+            else {
+                maxSum = max(maxSum, sum);
+                sum = 0;
+            }
+        }
+        maxSum = max(maxSum, sum);
+        return maxSum;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        int ar[n];
+        for (int i = 0; i < n; i++) {
+            cin >> ar[i];
+        }
+        Solution ob;
+        cout << ob.max_Books(ar, n, k) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
