802f152e347f79aad68b073b53ecaf6901dd10a1
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-05-30 18:17:24 -0700

    Create 31.cpp

---
+++ gfg/may/31/31.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int swapNibbles(int n) {
+        // code here
+        int right = (n & 0b00001111);
+        
+        right = (right << 4);
+        
+        int left = (n & 0b11110000);
+        
+        left = (left >> 4);
+        
+        return (right | left);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.swapNibbles(n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
