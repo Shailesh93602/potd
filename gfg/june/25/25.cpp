e97d552cb61e76c21641a722ad4ba97b6a6e5eef
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-06-25 07:32:22 +0530

    Create 25.cpp

---
+++ gfg/june/25/25.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<vector<int>> rotateMatrix(int k, vector<vector<int>> mat) {
+        // code here
+        int n = mat.size();
+        int m = mat[0].size();
+        k %= m;
+        if(k == 0) return mat;
+        vector<vector<int>> ans;
+        for(int i=0; i<n; i++) {
+            vector<int> temp;
+            for(int j=k; j<m; j++) {
+                temp.push_back(mat[i][j]);
+            }
+            for(int j=0; j<k; j++) {
+                temp.push_back(mat[i][j]);
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m, k;
+        cin >> n >> m >> k;
+        vector<vector<int>> mat(n, vector<int>(m));
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                cin >> mat[i][j];
+        Solution ob;
+        vector<vector<int>> ans = ob.rotateMatrix(k, mat);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++)
+                cout << ans[i][j] << " ";
+            cout << "\n";
+        }
+    }
+}
+
+// } Driver Code Ends
