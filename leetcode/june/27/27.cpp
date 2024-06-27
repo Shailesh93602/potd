8164309c195cdd3894d953b70966f5dada76dd47
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-06-27 07:39:02 +0530

    Create 27.cpp

---
+++ leetcode/june/27/27.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+        int first = edges[0][0];
+        int second = edges[0][1];
+        if(edges[1][0] == first || edges[1][1] == first) return first;
+        return second;
+    }
+};
