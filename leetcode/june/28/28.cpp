7b37957f2642d943869b7da519cb10ef554cae9c
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-06-28 12:12:21 +0530

    Create 28.cpp

---
+++ leetcode/june/28/28.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    long long maximumImportance(int n, vector<vector<int>>& roads) {
+        vector<int> degree(n, 0);
+
+        for(auto road: roads) {
+            degree[road[0]]++;
+            degree[road[1]]++;
+        }
+
+        vector<int> cities(n);
+        for(int i=0; i<n; i++) cities[i] = i;
+
+        sort(cities.begin(), cities.end(), [&](int a, int b) {
+            return degree[a] > degree[b];
+        });
+
+        long long ans = 0;
+        for(int i=0; i<n; i++) 
+            ans += (long long)(n-i) * degree[cities[i]];
+
+        return ans;
+    }
+};
