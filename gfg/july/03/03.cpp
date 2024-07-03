41fd22fa14cb9a9cf181bf414624190d84e1ce09
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-07-03 07:59:35 +0530

    Create 03.cpp

---
+++ gfg/july/03/03.cpp
@@ -0,0 +1,97 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Link list node */
+struct Node {
+    int data;
+    struct Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+/* Linked List node structure
+
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+*/
+
+class Solution {
+  public:
+    Node* removeAllDuplicates(struct Node* head) {
+        // code here
+        unordered_map<int, int> map;
+        Node* temp = head;
+        while(temp) {
+            map[temp->data]++;
+            temp = temp->next;
+        }
+        Node* dummy = new Node(-1);
+        Node* dummyPointer = dummy;
+        while(head) {
+            if(map[head->data] == 1) {
+                dummy->next = new Node(head->data);
+                dummy = dummy->next;
+            }
+            head = head->next;
+        }
+        return dummyPointer->next;
+    }
+};
+
+//{ Driver Code Starts.
+/* Function to print linked list */
+void printList(struct Node *head) {
+    struct Node *temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
+// Driver program to test above functions
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int N;
+
+        cin >> N;
+
+        Node *head = NULL;
+        Node *temp = head;
+
+        for (int i = 0; i < N; i++) {
+            int data;
+            cin >> data;
+            if (head == NULL)
+                head = temp = new Node(data);
+            else {
+                temp->next = new Node(data);
+                temp = temp->next;
+            }
+        }
+
+        Solution ob;
+        head = ob.removeAllDuplicates(head);
+        printList(head);
+
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
