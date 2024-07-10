04b95b882dc060986ee4bf26d30c86def9a5d15f
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-07-10 07:51:13 +0530

    Create 10.cpp

---
+++ gfg/july/10/10.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int maxSquare(int n, int m, vector<vector<int>> mat) {
+        // code here
+        int sub[n][m];
+        int maxSize = 0;
+        
+        for(int i=0; i<m; i++) {
+            sub[0][i] = mat[0][i];
+            maxSize = max(maxSize, sub[0][i]);
+        }
+        
+        for(int i=0; i<n; i++) {
+            sub[i][0] = mat[i][0];
+            maxSize = max(maxSize, sub[i][0]);
+        }
+        
+        for(int i=1; i<n; i++) {
+            for(int j=1; j<m; j++) {
+                if(mat[i][j] == 1) 
+                    sub[i][j] = 
+                        min(sub[i-1][j-1], min(sub[i][j-1], sub[i-1][j])) + 1;
+                else sub[i][j] = 0;
+                maxSize = max(maxSize, sub[i][j]);
+            }
+        }
+        
+        return maxSize;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> mat(n, vector<int>(m, 0));
+        for (int i = 0; i < n * m; i++)
+            cin >> mat[i / m][i % m];
+
+        Solution ob;
+        cout << ob.maxSquare(n, m, mat) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
