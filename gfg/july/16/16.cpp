d8adbe4d23372ca92be12b710940bdb33b60d923
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-07-16 07:31:58 +0530

    Create 16.cpp

---
+++ gfg/july/16/16.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+
+    string printString(string s, char ch, int count) {
+        // Your code goes here
+        int n = s.length();
+        int i = 0;
+        while(i < n) {
+            if(s[i++] == ch) count--;
+            if(!count) break;
+        }
+        if(!count) {
+            string ans = "";
+            while(i < n) {
+                ans += s[i++];
+            }
+            return ans;
+        }
+        return "";
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        char ch;
+        int count;
+
+        cin >> s >> ch >> count;
+        Solution ob;
+        cout << ob.printString(s, ch, count) << "\n";
+    }
+
+    return 0;
+}
+// } Driver Code Ends
