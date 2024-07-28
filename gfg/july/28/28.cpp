25c9ce1fb06dccd7294bbfa36966cae63c3461a3
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-07-28 09:59:50 +0530

    Create 28.cpp

---
+++ gfg/july/28/28.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+
+    string removeDups(string str) {
+        // Your code goes here
+        unordered_map<char, int> map;
+        string ans = "";
+        for(char c: str) {
+            if(map.find(c) == map.end()) {
+                ans += c;
+                map[c]++;
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+
+        Solution ob;
+        cout << ob.removeDups(s) << "\n";
+    }
+
+    return 0;
+}
+// } Driver Code Ends
