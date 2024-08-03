1fd5cfcad2fd8da5e509155fb1cc2492bb2bbfea
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-08-03 20:46:38 +0530

    Create 03.cpp

---
+++ gfg/august/03/03.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    // Function to find if there is a celebrity in the party or not.
+    int celebrity(vector<vector<int> >& mat) {
+        // code here
+        int n = mat.size();
+        
+        int i = 0;
+        int j = n-1;
+        
+        while(i < j) {
+            if(mat[i][j]) i++;
+            else j--;
+        }
+        
+        for(int k=0; k<n; k++) 
+            if((k != i) && (mat[i][k] || !mat[k][i])) 
+                return -1;
+        
+        return i;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<vector<int> > M(n, vector<int>(n, 0));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cin >> M[i][j];
+            }
+        }
+        Solution ob;
+        cout << ob.celebrity(M) << endl;
+    }
+}
+
+// } Driver Code Ends
