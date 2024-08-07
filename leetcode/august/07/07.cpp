9349d5a0f56d82227508bc3cbb1ac88982de01e3
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-08-07 08:02:19 +0530

    Create 07.cpp

---
+++ leetcode/august/07/07.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+     vector<pair<int, string>> mp = {
+    {1000000000, "Billion"},
+    {1000000, "Million"},
+    {1000, "Thousand"},
+    {100, "Hundred"},
+    {90, "Ninety"},
+    {80, "Eighty"},
+    {70, "Seventy"},
+    {60, "Sixty"},
+    {50, "Fifty"},
+    {40, "Forty"},
+    {30, "Thirty"},
+    {20, "Twenty"},
+    {19, "Nineteen"},
+    {18, "Eighteen"},
+    {17, "Seventeen"},
+    {16, "Sixteen"},
+    {15, "Fifteen"},
+    {14, "Fourteen"},
+    {13, "Thirteen"},
+    {12, "Twelve"},
+    {11, "Eleven"},
+    {10, "Ten"},
+    {9, "Nine"},
+    {8, "Eight"},
+    {7, "Seven"},
+    {6, "Six"},
+    {5, "Five"},
+    {4, "Four"},
+    {3, "Three"},
+    {2, "Two"},
+    {1, "One"}
+};
+    string numberToWords(int num) {
+        if(!num) return "Zero";
+
+        for(auto it: mp) {
+            if(num >= it.first) {
+                string temp = "";
+                if(num >= 100) temp = numberToWords(num / it.first) + " ";
+
+                string temp2 = it.second;
+                string temp3 = "";
+                if(num%it.first != 0) 
+                    temp3 = " " + numberToWords(num%it.first);
+
+                return temp + temp2 + temp3;
+            }
+        }
+        return "";
+    }
+};
