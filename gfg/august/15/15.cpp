13b6e993bec4af311a4808fbba263b59ee7fb8f9
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-08-15 08:39:13 +0530

    Create 15.cpp

---
+++ gfg/august/15/15.cpp
@@ -0,0 +1,104 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node* node) {
+    while (node != NULL) {
+        cout << node->data % 10;
+        node = node->next;
+    }
+    cout << "\n";
+}
+
+
+// } Driver Code Ends
+// User function template for C++
+
+/*
+
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+class Solution {
+  public:
+    int addCarry(Node* head) {
+        if(!head) return 1;
+        int temp = head->data += addCarry(head->next);
+        
+        head->data = temp%10;
+        return temp / 10;
+    }
+    Node* addOne(Node* head) {
+        // Your Code here
+        // return head of list after adding one
+        int c = addCarry(head);
+        
+        if(c) {
+            Node* temp = new Node(c);
+            temp->next = head;
+            return temp;
+        }
+        
+        return head;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        if (arr.empty()) {
+            cout << -1 << endl;
+            continue;
+        }
+
+        int data = arr[0];
+        struct Node* head = new Node(data);
+        struct Node* tail = head;
+        for (int i = 1; i < arr.size(); ++i) {
+            data = arr[i];
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+        Solution ob;
+        head = ob.addOne(head);
+        printList(head);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
