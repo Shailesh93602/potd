8be0b60a605d8efe36ac0b538191050bbaf83754
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-08-24 08:05:15 -0700

    Create 24.cpp

---
+++ leetcode/august/24/24.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    long long generatePalindromeFromLeft(long long leftHalf, bool isEvenLength) {
+        long long palindrome = leftHalf;
+        if (!isEvenLength) leftHalf /= 10;
+        while (leftHalf > 0) {
+            palindrome = palindrome * 10 + leftHalf % 10;
+            leftHalf /= 10;
+        }
+        return palindrome;
+    }
+    string nearestPalindromic(string n) {
+        long long number = stoll(n);
+        
+        if (number <= 10) return to_string(number - 1);
+        if (number == 11) return "9";
+        
+        if (n == "999999999999999999") {
+            return "1000000000000000001";
+        }
+        
+        int length = n.length();
+        long long leftHalf = stoll(n.substr(0, (length + 1) / 2));
+        
+        vector<long long> palindromeCandidates(5);
+        palindromeCandidates[0] = generatePalindromeFromLeft(leftHalf - 1, length % 2 == 0);
+        palindromeCandidates[1] = generatePalindromeFromLeft(leftHalf, length % 2 == 0);
+        
+        if (leftHalf < 999999999) {
+            palindromeCandidates[2] = generatePalindromeFromLeft(leftHalf + 1, length % 2 == 0);
+        } else {
+            palindromeCandidates[2] = stoll("1" + string(length - 1, '0') + "1");
+        }
+        
+        palindromeCandidates[3] = pow(10, length - 1) - 1;
+        palindromeCandidates[4] = pow(10, length) + 1;
+        
+        long long nearestPalindrome = 0;
+        long long minDifference = LLONG_MAX;
+        
+        for (long long candidate : palindromeCandidates) {
+            if (candidate == number) continue;
+            long long difference = abs(candidate - number);
+            if (difference < minDifference || (difference == minDifference && candidate < nearestPalindrome)) {
+                minDifference = difference;
+                nearestPalindrome = candidate;
+            }
+        }
+        
+        return to_string(nearestPalindrome);
+    }
+};
