2360f4bf69dd7a33780fd01f0802c0b4d433413a
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-08-24 08:03:05 -0700

    Create 24.cpp

---
+++ gfg/august/24/24.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to return max value that can be put in knapsack of capacity W.
+    int knapSack(int W, vector<int>& wt, vector<int>& val) {
+        // Your code here
+        int n = wt.size();
+        vector<int> dp(W + 1, 0);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = W; j >= wt[i]; j--) {
+                dp[j] = max(dp[j], dp[j - wt[i]] + val[i]);
+            }
+        }
+
+        return dp[W];
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    // taking total testcases
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        // reading number of elements and weight
+        int n, w;
+        vector<int> arr, val, wt, drr;
+        string ip;
+        int number;
+        getline(cin, ip);
+        stringstream ss(ip);
+
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        getline(cin, ip);
+        ss.clear();
+        ss.str(ip);
+
+        while (ss >> number) {
+            val.push_back(number);
+        }
+
+        w = arr[0];
+        n = val.size();
+        getline(cin, ip);
+        ss.clear();
+        ss.str(ip);
+
+        while (ss >> number) {
+            wt.push_back(number);
+        }
+        Solution ob;
+        cout << ob.knapSack(w, wt, val) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
