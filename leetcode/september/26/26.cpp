9420b94228a7f23858ae7e928816270016345551
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-09-26 08:18:01 +0530

    Create 26.cpp

---
+++ leetcode/september/26/26.cpp
@@ -0,0 +1,22 @@
+class MyCalendar {
+    vector<pair<int, int>> calendar;
+public:
+    MyCalendar() {
+        
+    }
+    
+    bool book(int start, int end) {
+        for(const auto [s, e]: calendar) 
+            if(start < e && s < end)
+                return false;
+        
+        calendar.emplace_back(start, end);
+        return true;
+    }
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
