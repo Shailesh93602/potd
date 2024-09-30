d584c038e1c6f6bda0056d3dae6e44a2656304ac
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-09-30 08:41:17 +0530

    Create 30.cpp

---
+++ leetcode/september/30/30.cpp
@@ -0,0 +1,40 @@
+class CustomStack {
+    list<int> stack;
+    int maxSize;
+public:
+    CustomStack(int maxSize) {
+        this->maxSize = maxSize;
+    }
+    
+      void push(int x) {
+        // Add the element to the top of the stack if it hasn't reached maxSize
+        if (stack.size() < maxSize) {
+            stack.push_back(x);
+        }
+    }
+
+    int pop() {
+        // Return -1 if the stack is empty, otherwise remove and return the top
+        // element
+        if (stack.empty()) return -1;
+        int topElement = stack.back();
+        stack.pop_back();
+        return topElement;
+    }
+
+    void increment(int k, int val) {
+        // Increment the bottom k elements (or all elements if k > stack size)
+        auto it = stack.begin();
+        for (int i = 0; i < k && it != stack.end(); i++, it++) {
+            *it += val;
+        }
+    }
+};
+
+/**
+ * Your CustomStack object will be instantiated and called as such:
+ * CustomStack* obj = new CustomStack(maxSize);
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * obj->increment(k,val);
+ */
