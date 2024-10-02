709fdf7db061776820f2aeb5198ec7bad4342bad
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-10-02 07:39:04 +0530

    Create 02.cpp

---
+++ gfg/october/02/02.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int rotateDelete(vector<int> &arr) {
+        // Your code here
+        int k = 1;
+        int n = arr.size();
+        
+        while(k <= (n/2)) {
+            int m = arr.size();
+            rotate(arr.begin(), arr.end()-1, arr.end());
+            arr.erase(arr.begin() + (m-k));
+            k++;
+        }
+        
+        return arr[0];
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        int res = obj.rotateDelete(arr);
+        cout << res << endl;
+        // string tl;
+        // getline(cin, tl);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
