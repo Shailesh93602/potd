9373ada3286713c27a8eaf1c31835fbe3342a7a5
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-10-09 08:21:32 +0530

    Create 09.cpp

---
+++ leetcode/october/09/09.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<char> st;
+        int count = 0;
+
+        for(int i=0; i<s.length(); i++) {
+            if(s[i] == '(') st.push(s[i]);
+            else if(st.empty()) count++;
+            else st.pop();
+        }
+
+        while(!st.empty()) {
+            count++;
+            st.pop();
+        }
+
+        return count;
+    }
+};
