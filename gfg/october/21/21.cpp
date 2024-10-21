69cfbe800bc254ba85e5b5770b0489142882f9e9
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-10-21 08:16:12 +0530

    Create 21.cpp

---
+++ gfg/october/21/21.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    int countgroup(vector<int>& arr) {
+        // Complete the function
+        int n = arr.size();
+        int xorVal = 0;
+        
+        for(int i: arr) xorVal ^= i;
+        
+        if(xorVal== 0) return (1 << (n-1)) -1;
+        
+        return 0;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        int ans = obj.countgroup(arr);
+        cout << ans << endl;
+    }
+}
+
+// } Driver Code Ends
