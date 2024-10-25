d829535cf55b538d1561964d8c4b577ad4bbf481
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-10-25 08:02:34 +0530

    Create 25.cpp

---
+++ gfg/october/25/25.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+
+  public:
+    vector<int> alternateSort(vector<int>& arr) {
+        // Your code goes here
+        vector<int> ans;
+        sort(arr.begin(), arr.end());
+        
+        int i=0, j=arr.size()-1;
+        while(i < j) {
+            ans.push_back(arr[j--]);
+            ans.push_back(arr[i++]);
+        }
+        
+        if(i == j) ans.push_back(arr[i]);
+        
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        vector<int> ans = obj.alternateSort(arr);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
