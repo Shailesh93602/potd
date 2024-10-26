e9cadced524320430069556e2ba6b858031f346b
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-10-25 22:47:32 -0700

    Update 26.cpp

--- leetcode/october/26/26.cpp
+++ leetcode/october/26/26.cpp
@@ -1,75 +1,47 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-struct Node {
-    int data;
-    struct Node *next;
-
-    Node(int x) {
-        data = x;
-        next = NULL;
-    }
-};
-
-
-// } Driver Code Ends
-/*
-  Node is defined as
-struct node
-{
-    int data;
-    struct node* next;
-
-    node(int x){
-        data = x;
-        next = NULL;
-    }
-}*head;
-*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
 class Solution {
-  public:
-    int count(struct Node* head, int key) {
-        // add your code here
-        int count = 0;
-        
-        while(head) {
-            if(head->data == key) count++;
-            head = head->next;
-        }
-        
-        return count;
+public:
+    map<int, int> left, right, height;
+    int heights(TreeNode* root) {
+        if (!root)
+            return 0;
+
+        int l = heights(root->left);
+        int r = heights(root->right);
+
+        left[root->val] = l;
+        right[root->val] = r;
+
+        return max(l, r) + 1;
+    }
+    void solve(TreeNode* root, int maxVal, int depth) {
+        if (!root)
+            return;
+
+        height[root->val] = maxVal;
+
+        solve(root->left, max(maxVal, depth + right[root->val]), depth + 1);
+        solve(root->right, max(maxVal, depth + left[root->val]), depth + 1);
+    }
+    vector<int> treeQueries(TreeNode* root, vector<int>& queries) {
+        heights(root);
+        solve(root->left, right[root->val], 1);
+        solve(root->right, left[root->val], 1);
+
+        vector<int> ans;
+        for(int i: queries) ans.push_back(height[i]);
+
+        return ans;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        struct Node *head = new Node(arr[0]);
-        struct Node *tail = head;
-        for (int i = 1; i < arr.size(); ++i) {
-            tail->next = new Node(arr[i]);
-            tail = tail->next;
-        }
-        int key;
-        cin >> key;
-        cin.ignore();
-        Solution ob;
-        cout << ob.count(head, key) << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
