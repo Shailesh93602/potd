d02fa8cfb104a087f0cf055a1b5d30906066eba7
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-10-26 19:44:54 -0700

    Create 27.cpp

---
+++ gfg/october/27/27.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    bool findTriplet(vector<int>& arr) {
+        // Your code
+        int n = arr.size();
+        
+        sort(arr.begin(), arr.end());
+        for(int i=n-1; i>=0; i--) {
+            int j = 0;
+            int k = i-1;
+            
+            while(j < k) {
+                if(arr[j]+arr[k] == arr[i]) return true;
+                else if(arr[j]+arr[k] < arr[i]) j++;
+                else k--;
+            }
+        }
+        
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        bool res = obj.findTriplet(arr);
+        if (res)
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+        // cout << res << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
