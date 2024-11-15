c878edea924c2f6412a00406f851f4114c71e8d5
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-11-15 08:10:51 +0530

    Create 15.cpp

---
+++ gfg/november/15/15.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    // Function returns the second
+    // largest elements
+    int getSecondLargest(vector<int> &arr) {
+        // Code Here
+        int first = -1, second = -1;
+        
+        for(int i: arr) {
+            if(i > first) {
+                second = first;
+                first = i;
+            }
+            else if(i > second && i != first) second = i;
+        }
+        
+        return second;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        int ans = ob.getSecondLargest(arr);
+        cout << ans << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
