af6314c90bce88e01e16f995304315762d99275f
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-11-18 08:46:16 +0530

    Create 18.cpp

---
+++ leetcode/november/18/18.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> decrypt(vector<int>& code, int k) {
+        int n = code.size();
+        vector<int> ans(n, 0);
+
+        if(k == 0) return ans;
+
+        int start=1, end=k, sum=0;
+
+        if(k < 0) {
+            start = n  - abs(k);
+            end = n-1;
+        }
+
+        for(int i=start; i<=end; i++) sum += code[i];
+        for(int i=0; i<n; i++) {
+            ans[i] = sum;
+            sum -= code[start%n];
+            sum += code[(end+1)%n];
+            start++;
+            end++;
+        }
+
+        return ans;
+    }
+};
