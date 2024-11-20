d64f9fe865e2f0c37696db05a2d766f6fe0e9e75
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-11-20 08:06:58 +0530

    Create 20.cpp

---
+++ gfg/november/20/20.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to find the majority elements in the array
+    vector<int> findMajority(vector<int>& arr) {
+        // Your code goes here.
+        vector<int> ans;
+        map<int, int> map;
+        
+        for(int i: arr) map[i]++;
+        
+        for(auto it=map.begin(); it!=map.end(); it++) {
+            if(it->second > arr.size()/3) ans.push_back(it->first);
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+
+        string s;
+        getline(cin, s);
+        stringstream ss(s);
+        vector<int> nums;
+        int num;
+        while (ss >> num) {
+            nums.push_back(num);
+        }
+        Solution ob;
+        vector<int> ans = ob.findMajority(nums);
+        if (ans.empty()) {
+            cout << "[]";
+        } else {
+            for (auto &i : ans)
+                cout << i << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
