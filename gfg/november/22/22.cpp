be2626e74cc1f5dacfa459d798a6ef93005be459
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-11-22 08:28:21 +0530

    Create 22.cpp

---
+++ gfg/november/22/22.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int maximumProfit(vector<int> &prices) {
+        // code here
+        int buy = prices[0];
+        int ans = 0;
+        
+        for(int i=1; i<prices.size(); i++) {
+            ans = max(ans, prices[i] - buy);
+            buy = min(buy, prices[i]);
+        }
+        
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> prices;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            prices.push_back(number);
+        }
+
+        Solution ob;
+        int ans = ob.maximumProfit(prices);
+        cout << ans << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
