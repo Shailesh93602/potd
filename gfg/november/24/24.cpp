91162a9f79a900d4bbdd951fc00b1df05dd53df4
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-11-24 08:48:13 +0530

    Create 24.cpp

---
+++ gfg/november/24/24.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    // Function to find the sum of contiguous subarray with maximum sum.
+    int maxSubarraySum(vector<int> &arr) {
+        // code here...
+        int maxSum = INT_MIN;
+        int sum = 0;
+        
+        for(int i: arr) {
+            sum += i;
+            maxSum = max(sum, maxSum);
+            if(sum < 0) sum = 0;
+        }
+        
+        return maxSum;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // To discard any leftover newline characters
+    while (t--)   // while testcases exist
+    {
+        vector<int> arr;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        cout << ob.maxSubarraySum(arr) << endl;
+    }
+}
+// } Driver Code Ends
