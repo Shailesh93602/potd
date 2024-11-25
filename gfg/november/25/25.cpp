256931d8719368fd3dd6f55b380e5f424aadfeff
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-11-25 08:25:58 +0530

    Create 25.cpp

---
+++ gfg/november/25/25.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    // Function to find maximum product subarray
+    int maxProduct(vector<int> &arr) {
+        // Your Code Here
+        int n = arr.size();
+        int minVal = arr[0], maxVal = arr[0], ans = arr[0];
+        
+        for(int i=1; i<n; i++) {
+            if(arr[i] < 0) swap(maxVal, minVal);
+            maxVal = max(arr[i], maxVal * arr[i]);
+            minVal = min(arr[i], minVal * arr[i]);
+            
+            ans = max(ans, maxVal);
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        // int n, i;
+        // cin >> n;
+        // vector<int> arr(n);
+        // for (i = 0; i < n; i++) {
+        //     cin >> arr[i];
+        // }
+
+        vector<int> arr;
+        string input;
+
+        // Read array
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        auto ans = ob.maxProduct(arr);
+        cout << ans << "\n";
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
