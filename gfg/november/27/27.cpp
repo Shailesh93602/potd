8e66658dcfee399790dc79a896c7233453a1a122
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-11-27 08:35:38 +0530

    Create 27.cpp

---
+++ gfg/november/27/27.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    // Function to find the smallest positive number missing from the array.
+    int missingNumber(vector<int> &arr) {
+        // Your code here
+        int n = arr.size();
+        sort(arr.begin(), arr.end());
+        
+        int i=0;
+        while(i < n && arr[i] < 1) i++;
+        
+        int j = 1;
+        while(i < n) {
+            if(arr[i] != j) return j;
+            while(arr[i] == j) i++;
+            j++;
+        }
+        
+        return j;
+    }
+};
+
+//{ Driver Code Starts.
+
+// int missingNumber(int arr[], int n);
+
+int main() {
+
+    // taking testcases
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        int result = ob.missingNumber(arr);
+        cout << result << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
