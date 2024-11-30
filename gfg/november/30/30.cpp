ee042fdbf98c8762d7fa2630cc64da090189169d
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-11-30 09:25:40 +0530

    Create 30.cpp

---
+++ gfg/november/30/30.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function is to check whether two strings are anagram of each other or not.
+    bool areAnagrams(string& s1, string& s2) {
+        // Your code here
+        vector<int> freq(26, 0);
+        
+        for(char c: s1) freq[c-'a']++;
+        for(char c: s2) freq[c-'a']--;
+        
+        for(int i: freq) if(i != 0) return false;
+        
+        return true;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+
+    cin >> t;
+
+    while (t--) {
+        string c, d;
+
+        cin >> c >> d;
+        Solution obj;
+        if (obj.areAnagrams(c, d))
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+        cout << "~" << endl;
+    }
+}
+
+// } Driver Code Ends
