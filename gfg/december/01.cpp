9dcc4ff362f0eb1b3eed2392027f1795505b3ddb
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-12-01 09:45:45 +0530

    Create 01.cpp

---
+++ gfg/december/01.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    // Function to find the first non-repeating character in a string.
+    char nonRepeatingChar(string &s) {
+        // Your code here
+        vector<int> freq(26, 0);
+        
+        for(char ch: s) freq[ch-'a']++;
+        
+        for(char ch: s) if(freq[ch-'a'] == 1) return ch;
+        
+        return '$';
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int T;
+    cin >> T;
+
+    while (T--) {
+
+        string S;
+        cin >> S;
+        Solution obj;
+        char ans = obj.nonRepeatingChar(S);
+
+        if (ans != '$')
+            cout << ans;
+        else
+            cout << "-1";
+
+        cout << endl;
+
+        cout << "~"
+             << "\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
