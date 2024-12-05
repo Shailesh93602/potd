f86247d049d6eb8a22551441836b8b0ce51f31d0
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-12-05 08:25:26 +0530

    Create 05.cpp

---
+++ gfg/december/05/05.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void sort012(vector<int>& arr) {
+        // code here
+        int count0 = 0, count1 = 0;
+        for(int i: arr) {
+            if(i == 0) count0++;
+            else if(i == 1) count1++;
+        }
+        
+        int i=0;
+        while(count0--) arr[i++] = 0;
+        while(count1--) arr[i++] = 1;
+        while(i < arr.size()) arr[i++] = 2;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+
+        vector<int> a;
+        string input;
+        int num;
+
+        getline(cin, input);
+        stringstream s2(input);
+        while (s2 >> num) {
+            a.push_back(num);
+        }
+        Solution ob;
+        ob.sort012(a);
+
+        int n = a.size();
+        for (int i = 0; i < n; i++) {
+            cout << a[i] << " ";
+        }
+
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
