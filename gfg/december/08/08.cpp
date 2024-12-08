4fb1190b83b554d731c2348e21b42932a4db5b2e
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-12-08 10:55:15 +0530

    Create 08.cpp

---
+++ gfg/december/08/08.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<vector<int>> mergeOverlap(vector<vector<int>>& arr) {
+        // Code here
+        sort(arr.begin(), arr.end());
+        
+        vector<vector<int>> ans;
+        ans.push_back(arr[0]);
+        
+        for(int i=1; i<arr.size(); i++) {
+            auto &last = ans.back();
+            
+            if(arr[i][0] <= last[1]) last[1] = max(last[1], arr[i][1]);
+            else ans.push_back(arr[i]);
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n;
+        cin >> n;
+        vector<vector<int>> arr(n);
+        for (int i = 0; i < n; i++) {
+            int x, y;
+            cin >> x >> y;
+            arr[i].push_back(x);
+            arr[i].push_back(y);
+        }
+        Solution obj;
+        vector<vector<int>> ans = obj.mergeOverlap(arr);
+        for (auto i : ans) {
+            for (auto j : i) {
+                cout << j << " ";
+            }
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
