757a971d41173ea2a1eae2499f4b591a418809c1
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-12-13 08:42:40 +0530

    Create 13.cpp

---
+++ gfg/december/13/13.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int findMin(vector<int>& arr) {
+        // complete the function here
+        int minVal = arr[0];
+        
+        for(int i=1; i<arr.size(); i++) 
+            if(arr[i] < minVal) 
+                minVal = arr[i];
+                
+        return minVal;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+
+    Solution ob;
+    while (t--) {
+        string line;
+        getline(cin, line);
+        stringstream ss(line);
+        vector<int> nums;
+        int num;
+        while (ss >> num) {
+            nums.push_back(num);
+        }
+        cout << ob.findMin(nums) << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
