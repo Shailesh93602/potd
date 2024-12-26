a300ed38f4bc5690152e07e702475ae8b0f33660
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-12-26 08:35:39 +0530

    Create 26.cpp

---
+++ gfg/december/26/26.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    bool twoSum(vector<int>& arr, int target) {
+        // code here
+        unordered_set<int> s;
+        
+        for(int i: arr) {
+            int x = target - i;
+            
+            if(s.find(x) != s.end()) return true;
+            
+            s.insert(i);
+        }
+        
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        int x;
+        cin >> x;
+        cin.ignore();
+
+        vector<int> arr;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        auto ans = ob.twoSum(arr, x);
+        cout << (ans ? "true" : "false") << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
