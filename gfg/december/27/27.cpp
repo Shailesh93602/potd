c077940b437910cfcd177638694274bcb4557033
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-12-27 08:20:10 +0530

    Create 27.cpp

---
+++ gfg/december/27/27.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int countPairs(vector<int> &arr, int target) {
+        // Code here
+        int count = 0;
+        unordered_map<int, int> map;
+        
+        for(int i: arr) {
+            int x = target - i;
+            
+            if(map.find(x) != map.end()) count += map[x];
+            
+            
+            map[i]++;
+        }
+        
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int target;
+        cin >> target;
+        cin.ignore();
+        Solution ob;
+        int res = ob.countPairs(arr, target);
+
+        cout << res << endl << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
