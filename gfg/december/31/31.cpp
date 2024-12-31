7fc654c3133652e26cad961d6ecceb15181a3945
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2024-12-31 08:10:55 +0530

    Create 31.cpp

---
+++ gfg/december/31/31.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+
+    // Function to return length of longest subsequence of consecutive integers.
+    int longestConsecutive(vector<int>& arr) {
+        // Your code here
+        unordered_set<int> s;
+        int ans = 0;
+        
+        for(int i: arr) s.insert(i);
+        for(int i: arr) {
+            if(s.find(i - 1) != s.end()) continue;
+            else {
+                int j = i;
+                while(s.find(j) != s.end()) j++;
+                ans = max(ans, j-i);
+            }
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+
+        // Read first array
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        int res = ob.longestConsecutive(arr);
+
+        cout << res << endl << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
