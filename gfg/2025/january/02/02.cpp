e119bcbeb304cc0f11091b3413040e1b34c3dcb1
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2025-01-02 08:42:56 +0530

    Create 02.cpp

---
+++ gfg/2025/january/02/02.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int countSubarrays(vector<int> &arr, int k) {
+        // code here
+        unordered_map<int, int> map;
+        int ans = 0, sum = 0;
+        
+        for(int i: arr) {
+            sum += i;
+            
+            if(sum == k) ans++;
+            if(map.find(sum - k) != map.end()) 
+                ans += map[sum - k];
+                
+            map[sum]++;
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        cout << obj.countSubarrays(arr, k);
+        cout << endl;
+        cout << "~"
+             << "\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
