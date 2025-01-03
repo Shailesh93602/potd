b278531b3212c27f1557b3a5795fb14e10c84425
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2025-01-03 22:54:01 +0530

    Create 03.cpp

---
+++ gfg/2025/january/03/03.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    long subarrayXor(vector<int> &arr, int k) {
+        // code here
+        int ans = 0, temp = 0;
+        unordered_map<int, int> map;
+        
+        for(int i: arr) {
+            temp ^= i;
+            ans += map[temp ^ k];
+            
+            if(temp == k) ans++;
+            
+            map[temp]++;
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int tc;
+    cin >> tc;
+    cin.ignore(); // Ignore the newline after the number of test cases
+
+    while (tc-- > 0) {
+        string input;
+        getline(cin, input); // Read the whole line for the array
+
+        stringstream ss(input);
+        vector<int> arr;
+        int num;
+
+        while (ss >> num) {
+            arr.push_back(num); // Push numbers into the vector
+        }
+
+        int k;
+        cin >> k;
+        cin.ignore(); // Ignore the newline after k
+
+        Solution obj;
+        cout << obj.subarrayXor(arr, k) << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
