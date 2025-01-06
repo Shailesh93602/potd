23b7ec7a36ea1268693cb1fc4d64c89878c3815c
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2025-01-06 08:22:37 +0530

    Create 06.cpp

---
+++ leetcode/2025/january/06/06.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> minOperations(string boxes) {
+        int n = boxes.size();
+        vector<int> ans(n, 0);
+        int leftBalls = 0, leftMoves = 0;
+        int rightBalls = 0, rightMoves = 0;
+
+        for(int i=0; i<n; i++) {
+            ans[i] += leftMoves;
+            leftBalls += boxes[i] - '0';
+            leftMoves += leftBalls;
+
+            int j = n-i-1;
+            ans[j] += rightMoves;
+            rightBalls += boxes[j] - '0';
+            rightMoves += rightBalls;
+        }
+
+        return ans;
+    }
+};
