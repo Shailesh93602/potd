6a74d8a58fa1452a08e5f3447468acf5c5b9c024
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2025-01-13 08:03:11 +0530

    Create 13.cpp

---
+++ gfg/2025/january/13/13.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+
+  public:
+    int maxWater(vector<int> &arr) {
+        // code here
+        int i=0, j=arr.size()-1, ans = 0;
+        
+        while(i < j) {
+            ans = max(ans, min(arr[i], arr[j]) * (j - i));
+            
+            if(arr[i] < arr[j]) i++;
+            else j--;
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+
+        // Read first array
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        int res = ob.maxWater(arr);
+
+        cout << res << endl << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
