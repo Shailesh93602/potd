6f3f4a2380bba7ad626462d29bf3ff9b0ebcad36
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2025-01-14 12:57:00 +0530

    Create 14.cpp

---
+++ gfg/2025/january/14/14.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to find equilibrium point in the array.
+    int findEquilibrium(vector<int> &arr) {
+        // code here
+        int sum = 0;
+        for(int i: arr) sum += i;
+        
+        int leftSum = 0;
+        for(int i=0; i<arr.size(); i++) {
+           sum -= arr[i];
+
+           if(sum == leftSum) return i;
+           
+           leftSum += arr[i];
+        }
+        
+        return -1;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // To discard any leftover newline characters
+    while (t--)   // while testcases exist
+    {
+        vector<int> arr;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        cout << ob.findEquilibrium(arr) << endl;
+        cout << "~" << endl;
+    }
+}
+// } Driver Code Ends
