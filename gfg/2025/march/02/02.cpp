a4f3a962f6ece0c3e7eac6c5a8c7367a47ae7a04
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2025-03-02 13:00:26 +0530

    Create 02.cpp

---
+++ gfg/2025/march/02/02.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    vector<int> maxOfSubarrays(vector<int>& arr, int k) {
+        // code here
+        vector<int> ans, window;
+        
+        for(int i=0; i<arr.size(); i++) {
+            while(!window.empty() && window.front() <= i-k) 
+                window.erase(window.begin());
+            while(!window.empty() && arr[i] >= arr[window.back()])
+                window.pop_back();
+            
+            window.push_back(i);
+            
+            if(i >= k-1) ans.push_back(arr[window.front()]);
+        }
+        
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // Ignore newline character after t
+
+    while (t--) {
+        vector<int> arr;
+        int k;
+        string inputLine;
+
+        getline(cin, inputLine); // Read the array input as a line
+        stringstream ss(inputLine);
+        int value;
+        while (ss >> value) {
+            arr.push_back(value);
+        }
+
+        cin >> k;
+        cin.ignore(); // Ignore newline character after k input
+
+        Solution obj;
+        vector<int> res = obj.maxOfSubarrays(arr, k);
+        for (int i = 0; i < res.size(); i++)
+            cout << res[i] << " ";
+        cout << endl;
+        cout << "~"
+             << "\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
