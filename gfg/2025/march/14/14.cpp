c24b768ace46361070b0b979a2641f1e531cce2b
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2025-03-14 10:35:09 +0530

    Create 14.cpp

---
+++ gfg/2025/march/14/14.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int count(vector<int>& coins, int sum) {
+        // code here.
+        int n = coins.size();
+        vector<int> ans(sum + 1);
+        ans[0] = 1;
+        
+        for(int i=0; i<n; i++) 
+            for(int j=coins[i]; j<=sum; j++)
+                ans[j] += ans[j - coins[i]];
+        
+        return ans[sum];
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int sum;
+        cin >> sum;
+        cin.ignore();
+        Solution ob;
+        cout << ob.count(arr, sum) << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
