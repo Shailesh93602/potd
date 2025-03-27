4156a134d0bfca1d48cf825ca40559c9b3e4be66
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2025-03-27 08:49:58 +0530

    Create 27.cpp

---
+++ gfg/2025/march/27/27.cpp
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    // Function to find the minimum number of platforms required at the
+    // railway station such that no train waits.
+    int findPlatform(vector<int>& arr, vector<int>& dep) {
+        // Your code here
+        int n = arr.size(), maxVal = dep[0];
+        
+        for(int i: dep) maxVal = max(maxVal, i);
+        
+        vector<int> res(maxVal + 2, 0);
+        
+        for(int i=0; i<n; i++) {
+            res[arr[i]]++;
+            res[dep[i] + 1]--;
+        }
+        
+        int count = 0, ans = 0;
+        
+        for(int i: res) {
+            count += i;
+            ans = max(ans, count);
+        }
+        
+        return ans;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr, dep;
+        string input;
+
+        // Read first array (arr)
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        // Read second array (dep)
+        getline(cin, input);
+        stringstream ss2(input);
+        while (ss2 >> number) {
+            dep.push_back(number);
+        }
+
+        // Assuming Solution class is defined elsewhere with a method findPlatform
+        Solution ob;
+        cout << ob.findPlatform(arr, dep);
+
+        cout << endl;
+        cout << "~\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
