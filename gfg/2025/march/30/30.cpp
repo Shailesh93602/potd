f12134f9491a83acf6c1618455ca6933a5361a24
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2025-03-30 12:43:03 +0530

    Create 30.cpp

---
+++ gfg/2025/march/30/30.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+// C++ program to find union of
+// two sorted arrays
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+/*You are required to complete this method*/
+class Solution {
+  public:
+    int startStation(vector<int> &gas, vector<int> &cost) {
+        // Your code here
+        int n = gas.size(), total = 0, curr = 0, ans = 0;
+        
+        for(int i=0; i<n; i++) {
+            int temp = gas[i] - cost[i];;;
+            curr += temp;
+            total += temp;
+            
+            if(curr < 0) {
+                curr = 0;
+                ans = i + 1;  
+            }
+        }
+        
+        if(total < 0) return -1;
+        
+        return ans;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+/* Driver program to test above function */
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+
+    while (t--) {
+
+        vector<int> gas, cost;
+        string input1;
+        getline(cin, input1);
+        stringstream ss(input1);
+        int number1;
+        while (ss >> number1) {
+            gas.push_back(number1);
+        }
+        string input2;
+        getline(cin, input2);
+        stringstream sss(input2);
+        int number2;
+        while (sss >> number2) {
+            cost.push_back(number2);
+        }
+        Solution ob;
+        int ans = ob.startStation(gas, cost);
+
+        cout << ans << endl;
+        cout << "~\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
