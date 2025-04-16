dca77c6b26a32e53cf01d6ace3f6848e55c69cac
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2025-04-16 08:19:55 +0530

    Create 16.cpp

---
+++ gfg/2025/april/16/16.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function template for C++
+
+class Solution {
+  public:
+    void floydWarshall(vector<vector<int>> &dist) {
+        // Code here
+        int n = dist.size();
+        
+        for(int i=0; i<n; i++) 
+            for(int j=0; j<n; j++) 
+                for(int k=0; k<n; k++) 
+                    dist[j][k] = min(dist[j][k], dist[j][i] + dist[i][k]);
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n;
+        cin >> n;
+        vector<vector<int>> matrix(n, vector<int>(n, -1));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cin >> matrix[i][j];
+            }
+        }
+        Solution obj;
+        obj.floydWarshall(matrix);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cout << matrix[i][j] << " ";
+            }
+            cout << "\n";
+        }
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
