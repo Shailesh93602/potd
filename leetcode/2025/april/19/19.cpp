8058d5e077573ca3f1a186d7028ca95343cc3b7b
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2025-04-19 09:32:35 +0530

    Create 19.cpp

---
+++ leetcode/2025/april/19/19.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    long long solve(vector<int> &nums, int val) {
+        int i = 0, j = nums.size() - 1;
+        long long ans = 0;
+
+        while(i < j) {
+            int sum = nums[i] + nums[j];
+
+            if(sum < val) {
+                ans += (j - i++);
+            }
+            else j--;
+        }
+
+        return ans;
+    }
+public:
+    long long countFairPairs(vector<int>& nums, int lower, int upper) {
+        sort(nums.begin(), nums.end());
+        
+        return solve(nums, upper + 1) - solve(nums, lower);
+    }
+};
