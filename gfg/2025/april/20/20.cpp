6ad8e4429c44afe3408d0e1e575a293845baf544
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2025-04-20 08:35:09 +0530

    Create 20.cpp

---
+++ gfg/2025/april/20/20.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function Template for C++
+class Solution {
+  public:
+    int findDuplicate(vector<int>& arr) {
+        // code here
+        int slow = arr[0], fast = arr[0];
+        
+        do {
+            slow = arr[slow];
+            fast = arr[arr[fast]];
+        } while(slow != fast);
+            
+        fast = arr[0];
+        
+        while(slow != fast) {
+            slow = arr[slow];
+            fast = arr[fast];
+        }
+        
+        return slow;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int test_cases;
+    cin >> test_cases;
+    cin.ignore();
+    while (test_cases--) {
+        string input;
+        vector<int> arr;
+
+        // Read the array from input line
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        // Solution instance to invoke the function
+        Solution ob;
+        int result = ob.findDuplicate(arr);
+
+        cout << (result) << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
