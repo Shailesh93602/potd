6d39e13eb7542c25c6a00ac60a98c8f902466da9
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2025-04-21 07:51:29 +0530

    Create 21.cpp

---
+++ gfg/2025/april/21/21.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int missingNum(vector<int>& arr) {
+        // code here
+        int ans1 = 0, ans2 = 0;
+        
+        for(int i: arr) ans1 ^= i;
+        for(int i=1; i<=arr.size() + 1; i++) ans2 ^= i;
+        
+        return ans1 ^ ans2;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // to ignore the newline after the integer input
+    while (t--) {
+        int n;
+        vector<int> a;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num);
+
+        Solution obj;
+        cout << obj.missingNum(a) << endl;
+        cout << "~\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
