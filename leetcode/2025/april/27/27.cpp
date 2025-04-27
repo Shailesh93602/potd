e76c6a481ffcc6ac4e74d0b8955634c22b0a2b6d
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2025-04-27 09:34:05 +0530

    Create 27.cpp

---
+++ leetcode/2025/april/27/27.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int countSubarrays(vector<int>& nums) {
+        int n = nums.size(), ans = 0;
+
+        for(int i=1; i<n-1; i++) if(nums[i] == (nums[i-1] + nums[i+1]) * 2) ans++;
+
+        return ans;
+    }
+};
