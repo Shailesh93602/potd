c8869dae145c6b85a3bd68e75be47f4321c0279c
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2025-05-01 08:45:22 +0530

    Create 01.cpp

---
+++ gfg/2025/may/01.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+    vector<int> nthRowOfPascalTriangle(int n) {
+        // code here
+        vector<int> ans;
+        int prev = 1;
+        ans.push_back(prev);
+        
+        for(int i=1; i<n; i++) {
+            int curr = (prev * (n - i)) / i;
+            ans.push_back(curr);
+            prev = curr;
+        }
+        
+        return ans;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+void printAns(vector<int> &ans) {
+    for (auto &x : ans) {
+        cout << x << " ";
+    }
+    cout << "\n";
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        auto ans = ob.nthRowOfPascalTriangle(n);
+        printAns(ans);
+
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
