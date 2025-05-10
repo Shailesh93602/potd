167b5ec7c80932fe39bfdae3a65c9c7c102f9aef
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2025-05-10 07:32:14 +0530

    Create 10/10.cpp

---
+++ leetcode/2025/may/10/10.cpp
@@ -0,0 +1,10 @@
+                zero1++;
+            }
+        }
+        for (int i : nums2) {
+            sum2 += i;
+            if (i == 0) {
+                sum2++;
+                zero2++;
+            }
+        }
