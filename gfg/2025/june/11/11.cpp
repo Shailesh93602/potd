8afdbe2f601839118074b305f255877e58773ce6
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2025-06-11 08:23:47 +0530

    Create 11.cpp

---
+++ gfg/2025/june/11/11.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    int findLength(vector<int> &color, vector<int> &radius) {
+        // code here
+        int n = color.size();
+        stack<int> st;
+        
+        for(int i=0; i<n; i++) {
+            if(!st.empty() && color[i] == color[st.top()] 
+                && radius[i] == radius[st.top()]) st.pop();
+            else st.push(i);
+        }
+    
+        return st.size();
+    }
+};
