5def001a5976142dd489d1a48fa225f49bd3d42b
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2025-06-21 08:50:25 +0530

    Create 21.cpp

---
+++ gfg/2025/june/21/21.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    int catchThieves(vector<char> &arr, int k) {
+        // Code here
+        int n = arr.size(), i = 0, j = 0, ans = 0;
+        
+        while(i < n && j < n) {
+            while(i < n && arr[i] != 'P') i++;
+            while(j < n && arr[j] != 'T') j++;
+            
+            if(i < n && j < n && abs(i - j) <= k) {
+                ans++;
+                i++;
+                j++;
+            } else if(j < n && j < i) j++;
+            else if(i < n && i < j) i++;
+        }
+        
+        return ans;
+    }
+};
