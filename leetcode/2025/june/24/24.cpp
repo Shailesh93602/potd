68738646653d5b79c073d8d930897a2f74f15a1a
Shaileshbhai Chaudhari <shailesh93602@gmail.com>
2025-06-24 07:28:54 +0530

    Create 24.cpp

---
+++ leetcode/2025/june/24/24.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
+        vector<int> ans;
+        int r = 0, n = nums.size();
+
+        for(int j=0; j<n; j++) {
+            if(nums[j] == key) {
+                int l = max(r, j - k);
+                r = min(n - 1, j + k) + 1;
+
+                for(int i=l; i<r; i++) ans.push_back(i);
+            }
+        }
+
+        return ans;
+    }
+};
